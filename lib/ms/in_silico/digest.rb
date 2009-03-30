de0b78be17692a437c254489dfa4be7af3d5db42
Simon Chiang <simon.a.chiang@gmail.com>
2009-03-30 16:02:21 -0600

    made digest allow fasta entry inputs

--- lib/ms/in_silico/digest.rb
+++ lib/ms/in_silico/digest.rb
@@ -25,6 +25,7 @@ module Ms
           raise ArgumentError, "unknown digester: #{digester}" 
         end
         
+        sequence = $1 if sequence =~ /\A>.*?\n(.*)\z/
         peptides = site_digest ? d.site_digest(sequence, max_misses): d.digest(sequence, max_misses)
         log 'digest', "#{sequence[0..10]}#{sequence.length > 10 ? '...' : ''} to #{peptides.length} peptides"
         peptides
--- test/ms/in_silico/digest_test.rb
+++ test/ms/in_silico/digest_test.rb
@@ -2,8 +2,16 @@ require File.join(File.dirname(__FILE__), '../../tap_test_helper.rb')
 require 'ms/in_silico/digest'
 
 class Ms::InSilico::DigestTest < Test::Unit::TestCase
+  include Ms::InSilico
   acts_as_script_test
-
+  
+  attr_accessor :d
+  
+  def setup
+    super
+    @d = Digest.new
+  end
+  
   def test_digest_documentation
     script_test(File.dirname(__FILE__) +  "../../../../") do |cmd|
       cmd.check "documentation", %q{
@@ -16,5 +24,25 @@ class Ms::InSilico::DigestTest < Test::Unit::TestCase
   - QQILSIMAG
 }
     end
+  end
+  
+  #
+  # process test
+  #
+  
+  def test_process_returns_array_of_peptide_fragments
+    assert_equal %w{
+      MIVIGR
+      SIVHPYITNEYEPFAAEK
+      QQILSIMAG
+    }, d.process("MIVIGRSIVHPYITNEYEPFAAEKQQILSIMAG")
+  end
+  
+  def test_process_skips_header_of_fasta_entries
+    assert_equal %w{
+      MIVIGR
+      SIVHPYITNEYEPFAAEK
+      QQILSIMAG
+    }, d.process(">header\nMIVIGRSIVHPYITNEYEPFAAEKQQILSIMAG")
   end
 end
